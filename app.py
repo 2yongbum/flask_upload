2644a27999288018abbe28e63cb93eecc35d541e
Yongbum Lee <2yongbum@gmail.com>
2021-12-08 23:12:22 +0900

    add dropzone.js

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ from flask import Flask, render_template, request, redirect, url_for, abort, \
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024
-app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif']
+app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
+app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg']
 app.config['UPLOAD_PATH'] = 'uploads'
 
 def validate_image(stream):
-    header = stream.read(512)  # 512 bytes should be enough for a header check
-    stream.seek(0)  # reset stream pointer
+    header = stream.read(512)
+    stream.seek(0)
     format = imghdr.what(None, header)
     if not format:
         return None
     return '.' + (format if format != 'jpeg' else 'jpg')
+
+@app.errorhandler(413)
+def too_large(e):
+    return "File is too large", 413
 
 @app.route('/')
 def index():
@@ -30,9 +34,9 @@ def upload_files():
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS'] or \
                 file_ext != validate_image(uploaded_file.stream):
-            abort(400)
+            return "Invalid image", 400
         uploaded_file.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-    return redirect(url_for('index'))
+    return '', 204
 
 @app.route('/uploads/<filename>')
 def upload(filename):
