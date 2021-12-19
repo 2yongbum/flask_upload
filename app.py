5b2b5cc2e9cc1652a365778442c49d464dcfef6f
Yongbum Lee <2yongbum@gmail.com>
2021-12-19 19:40:36 +0900

    add flask_wtf.file

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import os
 from flask import Flask, render_template, request, redirect, url_for, abort, \
     send_from_directory
 from werkzeug.utils import secure_filename
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+
 
 app = Flask(__name__)
 app.config['MAX_CONTENT_LENGTH'] = 2 * 1024 * 1024
 app.config['UPLOAD_EXTENSIONS'] = ['.jpg', '.png', '.gif', '.jpeg']
 app.config['UPLOAD_PATH'] = 'uploads'
+app.config['SECRET_KEY'] = 'you-will-never-guess'
 
 def validate_image(stream):
     header = stream.read(512)
@@ -39,5 +43,23 @@ def upload_files():
     return '', 204
 
 @app.route('/uploads/<filename>')
-def upload(filename):
+def uploads(filename):
     return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+class PhotoForm(FlaskForm):
+    photo = FileField(validators=[FileRequired(), FileAllowed(['jpg', 'png'], 'Images only!')])
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    form = PhotoForm()
+
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        f = form.photo.data
+        filename = secure_filename(f.filename)
+        f.save(os.path.join(
+            app.instance_path, 'photos', filename
+        ))
+        return redirect(url_for('index'))
+
+    return render_template('upload.html', form=form)
